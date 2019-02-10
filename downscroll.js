876c5152446bb8127116dfb7bc8a523ef84d5add
Jonas <jonas.tenweb@gmail.com>
2019-02-10 10:53:34 +0100

    First commit..
    Support for revealjs scrolldown and playlist in md-docs.

---
+++ downscroll.js
@@ -0,0 +1,35 @@
+// Enable scrolling in reveal..
+function resetSlideScrolling(slide) {
+    $(slide).removeClass('scrollable-slide');
+    $(slide).animate({ scrollTop: 0 }, 1000);
+    $('html').off("keydown");
+}
+
+function handleSlideScrolling(slide) {
+    if ($(slide).height() >= 800) {
+        $(slide).addClass('scrollable-slide');
+        $('html').on("keydown", function(e) {
+            if(e.shiftKey) {
+                if(e.keyCode == 40) { // down , up = 38
+                    let scrollVal = $(slide)[0].scrollTop + 400;
+                    //let scrollMax = $(slide)[0].scrollHeight - $(slide)[0].clientTop;
+                    //if(scrollVal > scrollMax) scrollVal = scrollMax;
+                    $(slide).stop(true, true).animate({ scrollTop: scrollVal },400);
+                } else if(e.keyCode == 38) {
+                    let scrollVal = $(slide)[0].scrollTop - 400;
+                    //if(scrollVal < 0) scrollVal = 0;
+                    $(slide).stop(true, true).animate({ scrollTop: scrollVal },400);
+                }
+            }
+        });
+    }
+}
+
+Reveal.addEventListener('ready', function (event) {
+    handleSlideScrolling(event.currentSlide);
+});
+
+Reveal.addEventListener('slidechanged', function (event) {
+    resetSlideScrolling(event.previousSlide)
+    handleSlideScrolling(event.currentSlide);
+});
