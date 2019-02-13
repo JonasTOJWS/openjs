d1931e3955e5e18d3079e9c55ea85059e067fbb4
Jonas <jonas.tenweb@gmail.com>
2019-02-13 18:57:47 +0100

    Update downscroll.js

--- docs/downscroll.js
+++ docs/downscroll.js
@@ -5,27 +5,24 @@ function resetSlideScrolling(slide) {
 }
 
 function handleSlideScrolling(slide) {
-    if ($(slide).height() >= $(windows).height()) {
+    if ($(slide).height() >= $(window).height()) {
         $(slide).addClass('scrollable-slide');
         $('html').on("keydown", function(e) {
             if(e.shiftKey) {
                 if(e.keyCode == 40) { // down , up = 38
                     let scrollVal = $(slide)[0].scrollTop + 400;
-                    //let scrollMax = $(slide)[0].scrollHeight - $(slide)[0].clientTop;
-                    //if(scrollVal > scrollMax) scrollVal = scrollMax;
                     $(slide).stop(true, true).animate({ scrollTop: scrollVal },400);
                 } else if(e.keyCode == 38) {
                     let scrollVal = $(slide)[0].scrollTop - 400;
-                    //if(scrollVal < 0) scrollVal = 0;
                     $(slide).stop(true, true).animate({ scrollTop: scrollVal },400);
                 }
             }
         });
-        $('html').on("keyup", function(e) {
-            if(e.shiftKey && (e.keyCode == 40 || e.keyCode == 38) { // down , up = 38
+        /*$('html').on("keyup", function(e) {
+            if(e.shiftKey && (e.keyCode == 40 || e.keyCode == 38)) { // down , up = 38
                 $(slide).stop(true, false);
             }
-        });
+        });*/
     }
 }
 
@@ -37,5 +34,3 @@ Reveal.addEventListener('slidechanged', function (event) {
     resetSlideScrolling(event.previousSlide)
     handleSlideScrolling(event.currentSlide);
 });
-
-Reveal.initialize();
