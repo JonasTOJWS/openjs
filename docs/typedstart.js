d58235df66eeb6656eda07baa8f1c50a1956c030
Jonas <jonas.tenweb@gmail.com>
2019-05-29 21:41:23 +0200

    Update typedstart.js

--- docs/typedstart.js
+++ docs/typedstart.js
@@ -1,17 +1,22 @@
 $(function() {    
     $(".typedjs").each(function(index, value) {
-        // Put text in <p> tags.. in div #typed-strings-id
-        // Add span with unique id
-        // Put all in div <div class="type-wrap">
+        let params = $(this).attr("params");
+        // Defaults..
+        let opts = { typeSpeed: 20, backSpeed: 0, backDelay: 500, startDelay: 300, loop: true };
+        if(params) {
+            for(let pair of params.split(",")) {
+                pair = pair.trim();
+                if(pair.includes(":")) {
+                    let p = pair.split(":")[0].trim();
+                    let v = pair.split(":")[1].trim();
+                    opts[p] = parseInt(v);
+                }
+            }
+        }
+
         let strings = $(this).find(".typed-strings")[0];
         let typeElem = $(this).find(".typed")[0];
-        var typed = new Typed(typeElem, {
-            stringsElement: strings,
-            typeSpeed: 20,
-            backSpeed: 0,
-            backDelay: 500,
-            startDelay: 300,
-            loop: true
-        });
+        opts.stringsElement = strings;
+        var typed = new Typed(typeElem, opts);
     });
 });
